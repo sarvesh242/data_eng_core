2a780b7130547327bb469dfd05bbaa380ae75d58
sarvesh242 <43514762+sarvesh242@users.noreply.github.com>
2025-02-04 18:01:59 +0530

    Update manage_snowflake_users.py

--- user_mangement/manage_snowflake_users.py
+++ user_mangement/manage_snowflake_users.py
@@ -1,1 +1,25 @@
+import yaml
+import snowflake.connector
 
+
+class user_mangement:
+  '''This class handles all functionalities related to user management in Snowflake'''
+  
+  def __init__(self,filepath):
+    ''' Constructor for the class user_mangement
+        Args: 
+          filepath: Path of the yml file containing the users'''
+    self.filepath = filepath
+    
+  def load_users_from_json(self):
+    '''Function to load all the users from the yml file.
+       returns: A list containing all the users'''
+    with open(,'rt') as fp:
+      yaml_data = yaml.load(fp, Loader=yaml.FullLoader)
+      print(yaml_data)
+      list_of_users = yaml_data['users']
+      # print(list_of_users)
+      return list_of_users
+  
+
+  
